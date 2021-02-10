06e731a5055f32777e51ec08c16e675c375eb91d
Aayushi82 <78746087+Aayushi82@users.noreply.github.com>
2021-02-10 15:16:41 +0530

    Create fahrenheittocelcius.c

---
+++ fahrenheittocelcius.c
@@ -0,0 +1,13 @@
+//program to convert degree fahrenheit to celcius
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float c,f;
+    printf("enter degree in fahrenheit");
+    scanf("%f",&f);
+    c=(f-32)*5.0/9.0;
+    printf("degree in celcius=%f",c);
+    getch();
+    return 0;
+}
