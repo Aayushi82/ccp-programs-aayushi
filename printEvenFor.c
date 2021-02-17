336c92b997405369017d82edc7ff8efc870161b2
Aayushi82 <78746087+Aayushi82@users.noreply.github.com>
2021-02-17 23:47:05 +0530

    Create printEvenFor.c

---
+++ printEvenFor.c
@@ -0,0 +1,23 @@
+//to print even no.from m to n using for loop
+#include<stdio.h>
+#include<conio.h>
+void printEven(int,int);
+void main()
+{
+    int m,n;
+    printf("enter the value of 2 nos. between which you want to print the even nos.\n");
+    scanf("%d %d",&m,&n);
+    printEven(m,n);
+    getch();
+}
+void printEven(int m,int n)
+{
+    int r,s=0;
+    for(int i=m;i<=n;i++)
+    {
+        if (i%2==0)
+        {
+            printf("\n %d",i);
+        }
+    }
+}
